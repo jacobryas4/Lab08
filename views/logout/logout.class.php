98f070aee8a664a1f5efe0537b737e91df05059a
AdamP <adammpatrick@yahoo.com>
2018-10-30 19:33:28 -0400

    All Skeletons created

--- views/logout/logout.class.php
+++ views/logout/logout.class.php
@@ -11,6 +11,11 @@ class Logout extends View{
         <div class="middle-row">
             <p>You have been successfully logged out.</p>
         </div>
+        
+        <div class="bottom-row">
+            <span style="float: left">Already have an account? <a href="index.php?action=login">Login</a></span>
+            <span style="float: right">Don't have an account? <a href="index.php">Register</a></span>
+        </div>
         <?php
         //diplay footer
         parent::footer();
---
+++ views/reset/reset.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class Reset extends View{
+    //display header
+    parent::header();
+    //******************How to check if the user is logged in?*******************
+}
