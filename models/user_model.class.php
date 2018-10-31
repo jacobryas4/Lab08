e555515d5896f18221cb7fe4eb7b5c63cfd42880
Jacob Bryant <jacobryas4@gmail.com>
2018-10-31 14:51:06 -0400

    Add files via upload
    finished a method and began work on another.  Still not finished

--- models/user_model.class.php
+++ models/user_model.class.php
@@ -20,5 +20,44 @@ class UserModel {
     // public methods 
     public function add_user() {
         
+        // get form field values 
+        $username    = $_POST["username"];
+        $password    = $_POST["password"];
+        $email       = $_POST["email"];
+        $firstName   = $_POST["firstname"];
+        $lastName    = password_hash($_POST["lastname"], PASSWORD_DEFAULT);
+        
+        // build SQL statement
+        // might have to add '' around variables
+        $sql = "INSERT INTO users (username, password, email, firstname, lastname) VALUES (" . $username . 
+                "," . $password . "," . $email . "," . $firstName . "," . $lastName . ")"; 
+        
+        // insert into table and return true/false depending on success
+        if ($this->dbConnect->query($sql) === true) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public function verify_user() {
+        
+        // retrieve values from form
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        
+        // build SQL statement
+        // START HERE
+        $sql = "SELECT * WHERE (username = '" . $username . "') AND ( password = '" . $password;
+        
+    }
+    
+    public function logout() {
+        
+    }
+    
+    public function reset_password() {
+        
     }
 }
